98bb95bfaf10a0f4be9edb6c8bf5e4f2e300892c
jaeheonkki <uj07096@naver.com>
2024-02-05 11:38:27 +0000

    ~pg42

---
+++ chap2_inner_sequence_type/0_declare_types.py
@@ -0,0 +1,16 @@
+#data type prac -> 데이터 타입별 선언하는 법
+
+l = []  #list 선언
+print(type(l))
+
+s = ""  #문자열 선언
+print(type(s))
+
+t = ()  #tuple 선언
+print(type(t))
+
+ba = bytearray(b"")  #바이트배열 선언
+type(ba)
+
+b = bytes([])  #바이트
+type(b)
